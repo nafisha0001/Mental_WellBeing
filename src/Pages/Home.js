55f88cb57160b2077f7d2a7e9ae7a3ee2eff54bf
Sumanpreet Kaur <suman3698kaur@gmail.com>
2024-09-11 16:41:19 +0530

    Added Footer

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Features from '../Components/Features'
 import './Home.css'
+import Footer from '../Components/Footer'
 
 function Home() {
   return (
     <div>
         <h1>Our Features</h1>
         <Features />
+        <Footer/>
     </div>
   )
 }
