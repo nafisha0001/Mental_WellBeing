558af4a148223fa43e256aa8afc17bb30903ffbf
nafishakhan0001@gmail.com <nafishakhan0001@gmail.com>
2024-10-16 10:14:05 +0530

    meditation and breathing

--- src/App.js
+++ src/App.js
@@ -10,21 +10,18 @@ import AboutUs from './Pages/AboutUs';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './Components/Footer';
 import BooksPage from './Pages/BooksPage';
-<<<<<<< HEAD
-// import VideoPlay from './Pages/VideoPlayer';
-=======
 
->>>>>>> 9b8e77b1e8d98f3579317fcaa79e03c6216adfe3
 import VideosPage from './Pages/VideosPage';
 import VideoPlayer from './Pages/VideoPlayer'; 
-<<<<<<< HEAD
+
 import MindfulMovement2 from './Pages/MindfulMovement2';
 import YogaDescription from './Pages/YogaDescription';
 import ExerciseDescription from './Pages/EcerciseDescription';
-=======
 import ArticlePage from './Pages/ArticlePage';
 import FullArticle from './Pages/FullArticle';
->>>>>>> 9b8e77b1e8d98f3579317fcaa79e03c6216adfe3
+import Meditation from './Pages/Meditation';
+import CalmBreating from './Pages/CalmBreathing';
+import MeditationVideo from './Pages/MeditationVideo';
 
 
 function App() {
@@ -40,19 +37,17 @@ function App() {
         <Route path="/wellness-library-books" element={<BooksPage />} />
         <Route path="/wellness-library-videos" element={<VideosPage/>} />
         <Route path="/video/:id" element={<VideoPlayer />} />
-<<<<<<< HEAD
-        <Route path="/wellness-library-article" element={<ArticlesPage/>} />
+
+        <Route path="/wellness-library-article" element={<ArticlePage/>} />
         <Route path="/mindful-movement" element={<MindfulMovement/>} />
         <Route path="/mindful-movement-2" element={<MindfulMovement2/>} />
         <Route path="/yoga-description" element={<YogaDescription/>} />
         <Route path="/exercise-description" element={<ExerciseDescription/>} />
-=======
-        
+        <Route path="/meditation" element={<Meditation />} />
+        <Route path="/calm-breathing" element={<CalmBreating />} />
         <Route path="/articles" element={<ArticlePage />} />
         <Route path="/article/:id" element={<FullArticle />} />
-
->>>>>>> c3b775112962c71d94c2c969e6a7d6484443d88b
->>>>>>> 9b8e77b1e8d98f3579317fcaa79e03c6216adfe3
+        <Route path="/meditation-video/:id" element={<MeditationVideo />} />
       </Routes>
       <Footer />
     </Router>
